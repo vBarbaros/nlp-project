bb5e161b620c642568e5f30952e70d7810cff717
Victor Barbaros <barbarosvictor@yahoo.com>
2018-11-15 18:46:04 -0500

    augmentation pipeline is working

--- aug_clean.py
+++ aug_clean.py
@@ -21,16 +21,21 @@ class MoviesReviewClassifier:
     def __init__(self):
         self.POS_REVIEW_FILE_PATH = os.path.join('rt-polaritydata', 'rt-polarity.pos')
         self.NEG_REVIEW_FILE_PATH = os.path.join('rt-polaritydata', 'rt-polarity.neg')
-        self.POS_TAGS_TO_AUG = ['JJ', 'JJR', 'JJS', 'RB', 'RBR', 'RBS'] 
+        self.POS_TAGS_TO_AUG = ['JJ']#, 'JJR', 'JJS']#, 'RB', 'RBR', 'RBS'] 
         self.pos_corpus = []
         self.neg_corpus = []
         self.test_corpus = []
         self.train_vs_test_cutoff = 0.7
         self.train_vs_valid_cutoff = 0.7
+
+        self.pos_aug_corpus = []
+        self.neg_aug_corpus = []
+
         self.mis_classification = []
         print 'Movies review classifier ready for work!!!'
 
     def read_data(self):
+        print 'READING DATA\n'
         # tokenizes each sentence/review, puts it in a separate list
         # and labels 'pos' or 'neg' after each list
         with open(self.POS_REVIEW_FILE_PATH, 'r') as sentences:
@@ -40,6 +45,10 @@ class MoviesReviewClassifier:
             self.neg_corpus = sentences.readlines()
 
     def split_data(self):
+        print 'SPLITTING DATA\n'
+        self.pos_corpus = self.pos_corpus + self.pos_aug_corpus
+        # self.neg_corpus = self.neg_corpus + self.neg_aug_corpus
+
         full_corpus = self.pos_corpus + self.neg_corpus
         y_all = ['pos']*len(self.pos_corpus) + ['neg']*len(self.neg_corpus)
 
@@ -65,45 +74,140 @@ class MoviesReviewClassifier:
         self.test_corpus = X_test
         return X_train, X_valid, y_train, y_valid, X_test, y_test
 
-    def augment_sentence(self, sentence):
-        synonyms = [] 
-        antonyms = [] 
-        tagged_sen = nltk.pos_tag(word_tokenize(sentence))
-        for item in tagged_sen:
-            if item[1] in self.POS_TAGS_TO_AUG:               
-                # print item[0], item[1], 
-                for syn in wn.synsets(item[0]): 
-                    for l in syn.lemmas(): 
-                        synonyms.append(l.name()) 
-                        if l.antonyms(): 
-                            antonyms.append(l.antonyms()[0].name())
+    def get_vectors_tfidf_vectorizer(self, params, train_sample, test_sample):
+        vectorizer = TfidfVectorizer(
+                                encoding='latin-1',
+                                token_pattern=r'[\w]+|[.,!?;]',
+                                ngram_range=(
+                                    params['ngram_min'] if params['ngram_min'] in params else 1, 
+                                    params['ngram_max'] if params['ngram_max'] in params else 2),
+                                stop_words=params['stop_words'] if params['stop_words'] in params else None, 
+                                min_df=params['min_df'] if params['min_df'] in params else 1,
+                                max_df = params['max_df'] if params['max_df'] in params else 1.0,
+                                max_features = params['max_features'],
+                                sublinear_tf=True,
+                                use_idf=True)
 
-        print ':::syno::: ', synonyms, '\n'
-        print ':::ante::: ', antonyms, '\n'
+        train_vetors = vectorizer.fit_transform(train_sample)
+        test_vectors = vectorizer.transform(test_sample)
+        return train_vetors, test_vectors, vectorizer
+
+    def get_best_params(self):
+        tune_lst = []
+        tune_lst.append({'ngram_min': 1,'ngram_max': 1,'stop_words': None, 'min_df': 3, 'max_df': 0.95, 'max_features':50000})
+        return tune_lst
+
+    def run_final_setup(self):
+        params_lst = self.get_best_params()
+        clr = MultinomialNB()
+        clr_key = 'Naive Bayes'
+        vectorizer = None
+
+        split_train_vs_test_cutoffs = [0.7]
+        split_train_vs_valid_cutoffs = [0.85]
+
+        X_train, X_valid, y_train, y_valid, X_test, y_test = [], [], [], [], [], []
+        max_vals = {'maxval': [0.2]}
+        for params in params_lst:
+            for i in split_train_vs_test_cutoffs:
+                self.train_vs_test_cutoff = i
+                for k in split_train_vs_valid_cutoffs:
+                    self.train_vs_valid_cutoff = k
+                    X_train, X_valid, y_train, y_valid, X_test, y_test = self.split_data()
+                    print len(X_train), len(X_valid), len(X_test)
+                    train_vecs, valid_vecs, vecter = self.get_vectors_tfidf_vectorizer(params, X_train, X_valid)
+                    vectorizer = vecter
+                    clr.fit(train_vecs, y_train)
+                    y_predicted = clr.predict(valid_vecs)
+                    acc = accuracy_score(y_valid, y_predicted)
+                    if acc > max_vals['maxval'][0]:
+                        max_vals['maxval'] = [acc, clr_key, i, k, params]
+                    print clr_key
+                    print 'Accuracy:', accuracy_score(y_valid, y_predicted)
+
+        print ':::::performance on testing sample:::::'
+        X_test_tr = vectorizer.transform(X_test)
+        y_predicted_test = clr.predict(X_test_tr)
+        print 'Accuracy:', accuracy_score(y_test, y_predicted_test)
+        print confusion_matrix(y_test, y_predicted_test)
+        # :::::preformance on testing sample:::::
+        # Accuracy: 0.78875
+        # [[1309  291]
+        # [ 385 1215]]
+
+        self.mis_classification = []
+        for i in range(0, len(y_predicted_test)):
+            if y_predicted_test[i] != y_test[i]:
+                self.mis_classification.append([y_predicted_test[i], y_test[i], X_test[i]])
+
+    def augment_sentence(self, sentence, corpus_polarity='pos'):
+        try:
+            tagged_sen = nltk.pos_tag(word_tokenize(sentence))
+        except UnicodeDecodeError:
+            return
+        aug_pos_sent = []
+        aug_neg_sent = []
+        neg_flag = False
+        pos_flag = False 
+
+        for i in range(len(tagged_sen)):
+            syno_lst = [] 
+            anto_lst = [] 
+            if tagged_sen[i][1] in self.POS_TAGS_TO_AUG:               
+                # tagged_sen[i][0], tagged_sen[i][1], 
+                try:
+                    for syn in wn.synsets(tagged_sen[i][0]): 
+                        for l in syn.lemmas(): 
+                            syno_lst.append(l.name().encode('utf-8'))
+                         
+                            if l.antonyms(): 
+                                anto_lst.append(l.antonyms()[0].name().encode('utf-8'))
+
+                except UnicodeDecodeError:
+                    return
+                if syno_lst != []:
+                    if corpus_polarity == 'pos':
+                        aug_pos_sent.append(syno_lst[0].encode('utf-8'))
+                    else:
+                        aug_neg_sent.append(syno_lst[0].encode('utf-8'))
+                    pos_flag = True
+                if anto_lst != []:
+                    if corpus_polarity == 'pos':
+                        aug_neg_sent.append(anto_lst[0].encode('utf-8'))
+                    else:
+                        aug_pos_sent.append(anto_lst[0].encode('utf-8'))
+                    neg_flag = True
+
+            else:
+                aug_pos_sent.append(tagged_sen[i][0])
+                aug_neg_sent.append(tagged_sen[i][0])
+        # print ':::syno::: ', syno_lst, '\n'
+        # print sentence, len(sentence.split()), '\n'
+        # print aug_pos_sent, " ".join(aug_pos_sent), len(aug_pos_sent), '\n'
+        # print aug_neg_sent, " ".join(aug_neg_sent), len(aug_neg_sent), '\n'
+        # print ':::anto::: ', anto_lst, '\n'
+        if pos_flag and (len(aug_pos_sent) >= len(sentence.split())):
+            self.pos_aug_corpus.append(" ".join(aug_pos_sent))
+        if neg_flag and (len(aug_neg_sent) >= len(sentence.split())):
+            self.neg_aug_corpus.append(" ".join(aug_neg_sent))
+
+    def augment_corpus(self):
+        print 'AUGMENTING DATA\n'
+        for s in self.pos_corpus:
+            self.augment_sentence(s, 'pos')
+
+        for s in self.neg_corpus:
+            self.augment_sentence(s, 'neg')
 
     def main(self):
         # phase 1: read data from files;
         self.read_data()
 
-        # for i in self.pos_corpus:
-        #     try:
-        #         print i, nltk.pos_tag(word_tokenize(i.encode('utf-8')))
-        #     except UnicodeDecodeError:
-        #         pass
-        
-        # Tags to look at:
-        # JJ(adjective), JJR(adj comparative), JJS(adj superlative)
-        # RB(adverb), RBR(adverb comparative), RBS(adverb superlative), WRB(Wh-adverb)-???,
+        # phase 2: augment data
+        self.augment_corpus()
 
-        
-        # print self.pos_corpus[0], nltk.pos_tag(word_tokenize(self.pos_corpus[0]))
-        # print self.pos_corpus[1], nltk.pos_tag(word_tokenize(self.pos_corpus[1]))
-        # print self.pos_corpus[2], nltk.pos_tag(word_tokenize(self.pos_corpus[2]))
-        self.augment_sentence(self.pos_corpus[0])
-        self.augment_sentence(self.pos_corpus[1])
-        self.augment_sentence(self.pos_corpus[2])
-
-        # print nltk.tag.pos_tag(word_tokenize('I am doing great.'))
+        # phase 3: run final train, with the optimal params and samples split ratios, as obtained in phase 3 & 4
+        self.run_final_setup()
 
 
 if __name__ == "__main__":

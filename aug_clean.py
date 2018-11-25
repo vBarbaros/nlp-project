6c24023fb7868703935c63820e10a7a2d83b0890
Victor Barbaros <barbarosvictor@yahoo.com>
2018-11-25 15:20:31 -0500

    minor comments to explain some params

--- aug_clean.py
+++ aug_clean.py
@@ -181,7 +181,7 @@ class MoviesReviewClassifier:
         
         # yeah, we can call the function above and instantiate this
         #slt == sent_len_threshold, ungs == use_n_grams_sents, nw == nth_word
-        slt_lst = [10, 20, 30, 0, 0, 0]
+        slt_lst = [10, 20, 30, 0] # if slt is not 0, then values of ungs DO NOT matter(check line 382 in augment_sentence() ), 
         ungs_lst = [1, 2, 3]
         nw_lst = [0, 1, 2, 3, -1]
 
